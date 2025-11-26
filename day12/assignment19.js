e05c424475b203b3ca204658b9b3b2aace44595a
sangeetadey99 <sdey6268@gmail.com>
2025-11-26 16:20:55 +0530

    Understanding Global and Function Execution Context

---
+++ day12/assignment19.js
@@ -0,0 +1,11 @@
+let age = 30;
+function displayAge() {
+  console.log("Current age is:", age);
+}
+function changeAge() {
+  age = age + 5;
+  console.log("Updated age is:", age);
+}
+displayAge();
+changeAge();
+displayAge();
