9368bda39c61c14fe4edab3952d6c94479333976
sangeetadey99 <sdey6268@gmail.com>
2025-11-30 01:27:43 +0530

    Understanding and Navigating the DOM

---
+++ day15/assignment11/script.js
@@ -0,0 +1,10 @@
+let heading = document.getElementById("mainHeading");
+heading.textContent = "Welcome to the DOM World!";
+
+let paragraphs = document.getElementsByTagName("p");
+for (let i = 0; i < paragraphs.length; i++) {
+  paragraphs[i].style.color = "blue";
+}
+
+let box = document.querySelector(".container");
+box.style.backgroundColor = "yellow";
